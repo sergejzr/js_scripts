21a4e289a423d335ce3c74d35cbe3d86df3ebb7e
Сергей <zerr@l3s.de>
2024-11-01 18:51:45 +0100

    Update Foocus-PasteClipboard.js

--- TamperMonkey/Foocus/Foocus-PasteClipboard.js
+++ TamperMonkey/Foocus/Foocus-PasteClipboard.js
@@ -7,7 +7,14 @@
 // @match        http://127.0.0.1:7865*
 // @grant        none
 // ==/UserScript==
+
+// License
+// This work is licensed under a Creative Commons Attribution 4.0 International License (CC BY 4.0).
+// By using this license, you are free to:
+// Share — copy and redistribute the material in any medium or format Adapt — remix, transform, and build upon the material for any purpose, even commercially. Attribution: You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the author endorses you or your use.
+// Enjoy a smoother image upload experience with Foocus Paster!   
 //Should teoretically work with many applications that base on svelte for image handling
+
 (function () {
     'use strict';
 
